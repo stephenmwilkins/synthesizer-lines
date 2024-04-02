dba80d16287ed54f23b14e1c00c11b278345ce58
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-04-02 11:47:52 +0100

    added cloudy tests

---
+++ analysis/cloudy_tests/full/bpass/run_all.sh
@@ -0,0 +1,14 @@
+export CLOUDY_DATA_PATH="/Users/sw376/Dropbox/Research/software/cloudy/c23.01/data/:./"
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 1
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 2
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 3
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 4
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 5
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 6
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 7
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 8
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 9
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 10
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 11
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 12
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 13
---
+++ analysis/cloudy_tests/relagn/run_all.sh
@@ -0,0 +1,6 @@
+export CLOUDY_DATA_PATH="/Users/sw376/Dropbox/Research/software/cloudy/c23.01/data/:./"
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 1
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 2
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 3
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 4
+/Users/sw376/Dropbox/Research/software/cloudy/c23.01/source/cloudy.exe -r 5
